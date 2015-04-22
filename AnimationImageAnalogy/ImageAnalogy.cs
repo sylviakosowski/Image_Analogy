94ec11b97f74875e52ac945caffcd3f2dd5aeaa9
Sylvia Kosowski <sylviakosowski@gmail.com>
2015-04-22 00:13:23 -0400

    Began writing patch matching algorithm

---
+++ AnimationImageAnalogy/ImageAnalogy.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AnimationImageAnalogy
+{
+    public class ImageAnalogy
+    {
+        /* Source pair */
+        private Color[,] imageA1;
+        private Color[,] imageA2;
+        private int patchDimension;
+
+        /* Constructor */
+        public ImageAnalogy(Color[,] imageA1, Color[,] imageA2, int patchDimension) 
+        {
+            this.imageA1 = imageA1;
+            this.imageA2 = imageA2;
+        }
+
+        /* Returns pixel at the top left corner of the patch in A1 which is the closest match
+         * to the current patch in B1. */
+        private Tuple<int,int> BestPatchMatch(Color[,] imageB1, Tuple<int,int> patchB1)
+        {
+            Tuple<int, int> bestPatchCoord = null;
+
+
+
+            return bestPatchCoord;
+        }
+
+        /* 
+         * Create an image analogy for the given image using the source pair.
+         */
+        public Color[,] CreateImageAnalogy(Color[,] imageB1)
+        {
+            int width = imageB1.GetLength(0);
+            int height = imageB1.GetLength(1);
+            Color[,] imageB2 = new Color[width, height];
+
+            /*Iterate through patches finding a best match for each */
+            for (int i = 0; i < width; i += patchDimension){
+                for (int j = 0; j < height; j += patchDimension) {
+                    
+                    
+                }
+            }
+
+            return imageB2;
+        }
+
+
+
+    }
+}
--- AnimationImageAnalogy/Program.cs
+++ AnimationImageAnalogy/Program.cs
@@ -13,10 +13,10 @@ namespace AnimationImageAnalogy
         {
             /* The source image pair */
             Color[,] imageA1 = Utilities.createImageArrayFromFile("TestImages/imageA1.png");
-            Color[,] imageB1 = Utilities.createImageArrayFromFile("TestImages/imageB1.png");
-
-            /* Image we want to generate a B out of */
             Color[,] imageA2 = Utilities.createImageArrayFromFile("TestImages/imageA2.png");
+
+            /* Image we want to generate a pair out of*/
+            Color[,] imageB1 = Utilities.createImageArrayFromFile("TestImages/imageB1.png");
 
             Console.ReadLine();
         }
